c486db389b002f66ec2b7ca4e5288ad50b1a2e45
Saron <161972402+Saronzeleke@users.noreply.github.com>
2025-03-01 21:04:32 -0800

    commit

--- src/app/components/position-list/position-list.component.ts
+++ src/app/components/position-list/position-list.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+// src/app/components/position-list/position-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { PositionService } from '../../services/position.service';
+import { TreeNode } from '../../utils/tree.utils';
+import { EmployeePosition } from '../../models/employee-position.model';
 
 @Component({
   selector: 'app-position-list',
-  imports: [],
   templateUrl: './position-list.component.html',
-  styleUrl: './position-list.component.scss'
+  styleUrls: ['./position-list.component.css'],
 })
-export class PositionListComponent {
+export class PositionListComponent implements OnInit {
+  treeData: TreeNode[] = [];
 
+  constructor(private positionService: PositionService) {}
+
+  ngOnInit(): void {
+    this.loadPositions();
+  }
+
+  loadPositions(): void {
+    this.positionService.getPositions().subscribe(positions => {
+      this.treeData = buildTree(positions);
+    });
+  }
+
+  onNodeClick(event: any): void {
+    const selectedPosition = event.node.origin;
+    console.log('Selected Position:', selectedPosition);
+  }
 }
